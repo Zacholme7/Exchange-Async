15ae76b362f94878e30a557d67fbebef64d6b453
Zachary Holme <zacharyholme@f4-d4-88-7a-94-b1.local.tld>
2023-11-22 17:36:38 -0600

    some work done

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -6,67 +6,43 @@ async fn main() {
     // event channel
     let (logger_tx, mut logger_rx) = tokio::sync::mpsc::unbounded_channel::<WebsocketEvent>();
 
-    // an unbounded channel to represent if we should close the connection????
+    // close signaler
     let (close_tx, mut close_rx) = tokio::sync::mpsc::unbounded_channel::<bool>();
 
     // spawn a new tokio thread to recieve message and close if we need to
     let wait_loop = tokio::spawn(async move {
         'hello: loop {
             select! {
-                event = logger_rx.recv() => println!("{event:?}"),
-                _ = close_rx.recv() => break 'hello
+                event = logger_rx.recv() => println!("{event:?}"), // this will give us all the logging messages
+                _ = close_rx.recv() => break 'hello // this will break us out if we have some closing logic
             }
         }
     });
 
     let streams: Vec<BoxFuture<'static, ()>> = vec![
-        Box::pin(market_websocket(logger_tx.clone())),
+        Box::pin(market_websocket(logger_tx.clone())), // clone the sender
     ];
 
-    /* 
-    let (logger_tx, mut logger_rx) = tokio::sync::mpsc::unbounded_channel::<WebsocketEvent>();
-    let (close_tx, mut close_rx) = tokio::sync::mpsc::unbounded_channel::<bool>();
-    let wait_loop = tokio::spawn(async move {
-        'hello: loop {
-            select! {
-                event = logger_rx.recv() => println!("{event:?}"),
-                _ = close_rx.recv() => break 'hello
-            }
-        }
-    });
-    // private api
-    //user_stream().await;
-    //user_stream_websocket().await;
-    // public api
-    let streams: Vec<BoxFuture<'static, ()>> = vec![
-        Box::pin(market_websocket(logger_tx.clone())),
-        Box::pin(kline_websocket(logger_tx.clone())),
-        Box::pin(all_trades_websocket(logger_tx.clone())),
-        Box::pin(last_price(logger_tx.clone())),
-        Box::pin(book_ticker(logger_tx.clone())),
-        Box::pin(combined_orderbook(logger_tx.clone())),
-        Box::pin(custom_event_loop(logger_tx)),
-    ];
-
-    for stream in streams {
+    for stream in streams{
         tokio::spawn(stream);
     }
 
     select! {
-        _ = wait_loop => { println!("Finished!") }
-        _ = tokio::signal::ctrl_c() => {
-            println!("Closing websocket stream...");
-            close_tx.send(true).unwrap();
-            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        _ = wait_loop => ( println!("finished"))
+        _ = tokio::signal::ctrl_c() {
+            println!("Closing websocket stream...;")
+            close_tx.send(true).unwrap(); // send close message to bdR
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await; // not sure why we are sleeping here
         }
     }
-    */
+
 }
 
-#[allow(dead_code)]
 async fn market_websocket(logger_tx: UnboundedSender<WebsocketEvent>) {
     let keep_running = AtomicBool::new(true); // Used to control the event loop
-    let agg_trade: String = agg_trade_stream("ethbtc");
+    let agg_trade: String = agg_trade_stream("ethbtc"); // this will configure the api ending for ethbtc for the aggredated trades
+    
+    // create websocket with handling event
     let mut web_socket: WebSockets<'_, WebsocketEvent> = WebSockets::new(|event: WebsocketEvent| {
         logger_tx.send(event.clone()).unwrap();
         match event {
@@ -85,7 +61,10 @@ async fn market_websocket(logger_tx: UnboundedSender<WebsocketEvent>) {
         Ok(())
     });
 
+    // connect to the websocket
     web_socket.connect(&agg_trade).await.unwrap(); // check error
+
+    // main event loop
     if let Err(e) = web_socket.event_loop(&keep_running).await {
         println!("Error: {e}");
     }
